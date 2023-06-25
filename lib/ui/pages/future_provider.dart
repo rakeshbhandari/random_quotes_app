fbadf47094e73b8de3f36f1de8bffe1978d9c950
Rakesh <rakeshbhandari@gmail.com>
2023-06-25 12:59:25 +0545

    ok

--- lib/ui/pages/future_provider.dart
+++ lib/ui/pages/future_provider.dart
@@ -50,6 +50,18 @@ class FutureProviderPage extends ConsumerWidget {
                 },
                 child: const Text('Get Random Quote'),
               ),
+              const SizedBox(height: 100),
+              Text(state is DataLoadingState ? 'Loading' : 'LOADED'),
+              const SizedBox(height: 40),
+              Icon(state is DataLoadingState
+                  ? Icons.coronavirus
+                  : Icons.health_and_safety),
+              Card(
+                  elevation: 5,
+                  color: Colors.teal,
+                  margin: const EdgeInsets.all(20),
+                  child: Text(
+                      (state is DataLoadingState ? 'Loadingggggg' : 'Loaded')))
             ],
           ),
         )));
